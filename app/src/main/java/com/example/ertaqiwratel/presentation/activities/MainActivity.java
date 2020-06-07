d510399bc26ffbc62cd82b0c5e0193a9c7fd8266
GemiDroid <eng.ahmedgemi@gmail.com>
2020-06-07 21:18:58 +0200

    Init Project structure.

--- app/src/main/java/com/example/ertaqiwratel/presentation/activities/MainActivity.java
+++ app/src/main/java/com/example/ertaqiwratel/presentation/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.ertaqiwratel;
+package com.example.ertaqiwratel.presentation.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.ertaqiwratel.R;
 
 public class MainActivity extends AppCompatActivity {
 
