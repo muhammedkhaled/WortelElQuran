0862682da8a347936493a0540bb24c39a81143f8
muhammad <muhammedkhaleda@gmail.com>
2020-06-14 19:53:14 +0200

    revert registration commit

--- app/src/main/java/com/example/ertaqiwratel/NavigationHost.java
+++
@@ -1,7 +0,0 @@
-package com.example.ertaqiwratel;
-
-import androidx.fragment.app.Fragment;
-
-public interface NavigationHost {
-    void navigateTo(Fragment fragment, boolean addToBackstack);
-}
--- app/src/main/java/com/example/ertaqiwratel/presentation/activities/MainActivity.java
+++ app/src/main/java/com/example/ertaqiwratel/presentation/activities/MainActivity.java
@@ -6,35 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.ertaqiwratel.NavigationHost;
-import com.example.ertaqiwratel.PhoneNumberFragment;
 import com.example.ertaqiwratel.R;
 
-public class MainActivity extends AppCompatActivity implements NavigationHost {
+public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.container_host, new PhoneNumberFragment())
-                    .commit();
-        }
-
-    }
-
-    @Override
-    public void navigateTo(Fragment fragment, boolean addToBackstack) {
-        FragmentTransaction transaction = getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.container_host, fragment);
-        if (addToBackstack) {
-            transaction.addToBackStack(null);
-        }
-        transaction.commit();
 
 
     }
